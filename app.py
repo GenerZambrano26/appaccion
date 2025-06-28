f9071914964a413f0b9e8cb87d1f053c6b7ab522
GenerZambrano26 <90639782+GenerZambrano26@users.noreply.github.com>
2025-06-27 22:38:04 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+﻿from flask import Flask, request, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/precio', methods=['GET'])
+def obtener_precio():
+    ticker = request.args.get('ticker')  # Captura el parámetro ?ticker=GOOGL
+
+    if not ticker:
+        return jsonify({'error': 'Debe proporcionar un ticker'}), 400
+
+    try:
+        data = yf.Ticker(ticker)
+        precio = data.history(period='1d')['Close'].iloc[-1]
+        return jsonify({
+            'accion': ticker,
+            'precio': round(precio, 2)
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
