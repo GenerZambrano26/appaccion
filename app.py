6babfe60a98a63230766a935153f5a50bf47db7b
GenerZambrano26 <90639782+GenerZambrano26@users.noreply.github.com>
2025-06-27 23:37:54 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ﻿from flask import Flask, request, jsonify
 import yfinance as yf
+import os
 
 app = Flask(__name__)
 
@@ -19,3 +20,7 @@ def obtener_precio():
         })
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
